0b278d4c24320c292d33878866a266821bd4cb5e
Nhat Nguyen <>
2022-04-01 11:00:57 +0700

    init project

---
+++ RabbleWabble/RabbleWabble/QuestionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RabbleWabble
+//
+//  Created by Nhat Nguyen on 4/1/22.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
